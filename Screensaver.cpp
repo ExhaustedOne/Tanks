f39cd4860abe191a63d9febe628bdf309951bd88
Nathan <magmaboy228@gmail.com>
2024-05-17 21:42:21 +0300

    Added Screensaver and AllegroApp classes

---
+++ Screensaver.cpp
@@ -0,0 +1,21 @@
+#include "AllegroBase.hpp"
+#include "Figure.cpp"
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+class Screensaver {
+vector<Figure> figures;
+
+void addFigure(Figure f){
+    figures.push_back(f);
+}
+
+void drawFigures(){
+    for (vector<Figure>::iterator it = figures.begin(); it != figures.end(); it++)
+        it->draw();
+}
+
+
+};
